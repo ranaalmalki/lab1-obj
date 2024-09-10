4cb52fc91f97eb35ba4fcb0fac041a1973e2017a
Rana Almalki <ranaalmalki12@gmail.com>
2024-09-10 10:59:19 +0300

    solve the queation not sure about the function

---
+++ main.js
@@ -0,0 +1,75 @@
+
+
+const User ={
+name:"Rana",
+email:"ranaalmalki12@gmail.com",
+id:26,
+Available:true,
+cheack:function(){
+    if(this.Available == true){
+        console.log(this.Available+" available");
+    }else{
+        console.log("not avalibale");
+        
+    }
+},
+addres:{
+    street:"hamed",
+    city:{
+        city:"riyadh",
+        city2:"jeddah"
+
+    },
+    
+    number:2,
+},
+skils:["problem solvine ","flexiblilty","abilit to work in ateam"]
+
+}
+
+//================
+console.log(User);
+
+//================
+
+console.log(User.name);
+
+//===============
+
+console.log(User.id = 6);
+console.log(User);
+
+
+//===============
+
+console.log(delete User.email)
+console.log(User);
+
+//===============
+ console.log(User.cheack());//not تغيير القسمه وتتاكد
+
+
+//===============
+console.log(User.addres);
+
+//===============
+
+console.log(User.addres.city.city2);
+
+
+//===============
+
+console.log(User.skils[2]);
+
+//===============
+console.log(User.skils.join(", "));
+
+
+
+
+
+
+
+
+
+
